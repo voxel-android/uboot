voxel-android/uboot

commit 971dbde2b87eb90221ea6c7f7bb4fb8aa9cd2389

PD#89216: mx tf card burning improve when 'bmp display' and 'erase boot'

drivers/usb/gadget/v2_burning/v2_usb_tool/optimus_transform.c
@@ -1,6 +1,7 @@
 #include "../v2_burning_i.h"
 #include "usb_pcd.h"
 #include "platform.h"
+#include <partition_table.h>
 
 #define MYDBG(fmt ...) printf("OPT]"fmt)
 
@@ -307,8 +308,6 @@ int optimus_mem_md (int argc, char * const argv[], char *info)
 
 int set_low_power_for_usb_burn(int arg, char* buff)
 {
-    int ret = 0;
-
     if(OPTIMUS_WORK_MODE_USB_PRODUCE == optimus_work_mode_get()){
         return 0;//just return ok as usb producing mode as LCD not initialized yet!
     }
@@ -342,13 +341,13 @@ int set_low_power_for_usb_burn(int arg, char* buff)
 int optimus_erase_bootloader(char* info)
 {
     int ret = 0;
+    extern int device_boot_flag;
 
 #if ROM_BOOT_SKIP_BOOT_ENABLED
     optimus_enable_romboot_skip_boot();
 #else
-    if(optimus_storage_init(0)){
-        DWN_ERR("Fail in storage_init\n");
-        //return __LINE__;
+    if(SPI_EMMC_FLAG == device_boot_flag || SPI_NAND_FLAG == device_boot_flag){
+        run_command("sf probe 2", 0);
     }
     ret = store_erase_ops((u8*)"boot", 0, 0, 0);
 #endif// #if ROM_BOOT_SKIP_BOOT_ENABLED

board/amlogic/configs/m6_mbx_v2.h
@@ -257,7 +257,7 @@
         "echo update...; run usb_burning; "\
         "if mmcinfo; then "\
             "if fatexist mmc 0 ${sdcburncfg}; then "\
-                "run sdc_burn ${sdcburncfg}; "\
+                "sdc_burn ${sdcburncfg}; "\
             "else "\
                 "if fatload mmc 0 ${loadaddr} aml_autoscript; then autoscr ${loadaddr}; fi;"\
                 "run recovery;"\

commit 08c4129c0c9960334379c02d2be5bb304cc89a54

Conditionally define "ret" variable to fix meson8 broken build.
Build is broken by 971dbde2b87eb90221ea6c7f7bb4fb8aa9cd2389

"ret" used in the conditional compilation. Declear variable conditionally,
ugly but fix the build.

drivers/usb/gadget/v2_burning/v2_usb_tool/optimus_transform.c
@@ -308,6 +308,10 @@ int optimus_mem_md (int argc, char * const argv[], char *info)
 
 int set_low_power_for_usb_burn(int arg, char* buff)
 {
+#if defined CONFIG_VIDEO_AMLLCD || USB_BURN_POWER_CONTROL
+    int ret = 0;
+#endif // defined CONFIG_VIDEO_AMLLCD || USB_BURN_POWER_CONTROL
+
     if(OPTIMUS_WORK_MODE_USB_PRODUCE == optimus_work_mode_get()){
         return 0;//just return ok as usb producing mode as LCD not initialized yet!
     }
@@ -315,7 +319,7 @@ int set_low_power_for_usb_burn(int arg, char* buff)
 #if defined(CONFIG_VIDEO_AMLLCD)
     //axp to low power off LCD, no-charging
     MYDBG("To close LCD\n");
-    ret = run_command("video dev disable", 0);
+    int ret = run_command("video dev disable", 0);
     if(ret){
         if(buff) sprintf(buff, "Fail to close back light");
         printf("Fail to close back light\n");